1cb25b763e10ae48cc2afdc54b14ec5d9b1f768d
ElideZavala <zavalavinagreelide@gmail.com>
2021-07-05 08:03:57 -0600

    function randomFunc

--- Password-Generator/script.js
+++ Password-Generator/script.js
@@ -1,3 +1,10 @@
+const randomFunc = {
+	lower: getRandomLower,
+	upper: getRandomUpper,
+	number: getRandomNumber,
+	symbol: getRandomSymbol
+}
+
 function getRandomLower() {
 		return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
@@ -6,16 +13,12 @@ function getRandomUpper() {
 	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
-function getRandomNumber(function getRandomNumber() {
-	return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}) {
+function getRandomNumber() {
 	return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
 function getRandomSymbol() {
-	return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
+	const symbols = '!@#$%^&*()[]=<>/,.{}'
+	return symbols[Math.floor(Math.random() * symbols.length)]
 
-console.log(getRandomLower());
-console.log(getRandomUpper());
-console.log(getRandomNumber());
+}
