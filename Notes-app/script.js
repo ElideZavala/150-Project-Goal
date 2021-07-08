a831ee1a0419187a62f3daefff31ec523d6aa04c
ElideZavala <zavalavinagreelide@gmail.com>
2021-07-08 10:15:40 -0600

    Funcionalidad

---
+++ Notes-app/script.js
@@ -0,0 +1,42 @@
+const addBtn = document.getElementById('add');
+
+addBtn.addEventListener('click', () => addNewNote());
+
+function addNewNote() {
+	const note = document.createElement('div')
+	note.classList.add('note');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{
+	/* <div class="tools">
+				<button class="edit"><i class="fas fa-edit"></i></button>
+				<button class="delete"><i class="fas fa-trash-alt"></i></button>
+			</div>
+
+			<div class="main hidden"></div>
+			<textarea></textarea> */
+}
+
+// 	<div class="note">
+// 	<div class="tools">
+// 		<button class="edit"><i class="fas fa-edit"></i></button>
+// 		<button class="delete"><i class="fas fa-trash-alt"></i></button>
+// 	</div>
+
+// 	<div class="hidden"></div>
+// 	<textarea></textarea>
+// </div>
