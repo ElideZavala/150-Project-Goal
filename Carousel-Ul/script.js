df0e82937e6b83e99e08043004f60f6d65ff6a33
ElideZavala <zavalavinagreelide@gmail.com>
2021-07-15 21:01:49 -0600

    transitions

--- Carousel-Ul/script.js
+++ Carousel-Ul/script.js
@@ -1,3 +1,26 @@
 const imgs = document.querySelector('#imags');
-const leftBtn = document.querySelector('#left')
-const rightBtn = document.querySelector('#right')
+const leftBtn = document.querySelector('#left');
+const rightBtn = document.querySelector('#right');
+
+const img = document.querySelectorAll('#imags img');
+
+console.log(img);
+
+let idx = 0;
+
+let interval = setInterval(run, 2000);
+
+function run() {
+	idx++;
+	changeImage()
+}
+
+function changeImage() {
+	if(idx > img.length - 1) {
+		idx = 0;
+	} else if(idx < 0) {
+		idx = img.length -1;
+	}
+
+	imgs.style.transform = `translateX(${-idx * 500}px)`
+}
