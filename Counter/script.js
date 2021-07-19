5eedc3c38550c4b16e46792003bd43a931e90511
ElideZavala <zavalavinagreelide@gmail.com>
2021-07-19 16:37:59 -0600

    modificaciones del HTML

--- Counter/script.js
+++ Counter/script.js
@@ -1,1 +1,17 @@
-console.log("Hola Mundo")
+const contar = document.querySelector(".count");
+const add = document.querySelector(".add");
+const resetear = document.querySelector(".resetear");
+const sub = document.querySelector(".substract");
+const botones = document.querySelector(".buttons");
+
+add.addEventListener('click', () => {
+	contar.innerHTML++; // <== sumara las veces que le demos click
+});
+
+sub.addEventListener('click', () => {
+	contar.innerHTML--; // <== se contara de forma regresiva. 
+});
+
+resetear.addEventListener('click', () => {
+	contar.innerHTML = 0;
+})
