ae20d4815bcc9f62e14362ae58d74d458f138a25
ElideZavala <zavalavinagreelide@gmail.com>
2021-07-23 17:41:43 -0600

    color random

--- Random-Hex-color-Generator/script.js
+++ Random-Hex-color-Generator/script.js
@@ -1,1 +1,4 @@
-console.log("Hola Mundo")
+let color = Math.random();
+color = Math.random().toString(16).substring(2, 9);  // <== pasara de cero a nueve y luego de E a F.  // empezamos desde el 2 t terminamos
+
+console.log(color);
