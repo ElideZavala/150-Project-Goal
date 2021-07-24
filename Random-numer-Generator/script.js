db51306c062f6e19fd120f11fc8c0ad74ee195d1
ElideZavala <zavalavinagreelide@gmail.com>
2021-07-24 14:34:04 -0600

    modificaciones del style

--- Random-numer-Generator/script.js
+++ Random-numer-Generator/script.js
@@ -2,7 +2,6 @@ const number = document.querySelector(".number");
 const btn = document.querySelector(".generate");
 
 const generarNumero = () => {
-	// Generar numero btn de 1 a 10
 	const randomNumber =  Math.floor(Math.random() * 10 + 1);
 	number.innerHTML = randomNumber;
 }
