9718f22b73f510a7a55de0e34b271b5f41059254
ElideZavala <zavalavinagreelide@gmail.com>
2021-07-23 18:32:20 -0600

    formula

--- Random-Hex-color-Generator/script.js
+++ Random-Hex-color-Generator/script.js
@@ -1,4 +1,11 @@
-let color = Math.random();
-color = Math.random().toString(16).substring(2, 9);  // <== pasara de cero a nueve y luego de E a F.  // empezamos desde el 2 t terminamos
+const hex = document.querySelector(".hex");
+const btn = document.querySelector(".generate");
 
-console.log(color);
+const generarColor = () => {
+	const randomColor = `#${Math.random().toString(16).substring(2, 8)}`;
+	document.body.style.backgroundColor = randomColor; 
+	hex.innerHTML = randomColor;
+}
+
+
+btn.addEventListener('click', generarColor);
