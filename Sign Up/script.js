4ef580b26084f51a67e3f4134e1a44239bbb6c92
ElideZavala <zavalavinagreelide@gmail.com>
2021-07-27 09:11:29 -0600

    click en Iniciar Sesion

--- Sign Up/script.js
+++ Sign Up/script.js
@@ -1,1 +1,10 @@
-console.log("Hola Mundo")
+const btn = document.querySelector('.banner-btn');
+const banner = document.querySelector('.banner');
+const formContainer = document.querySelector('.form-container');
+ 
+btn.addEventListener('click', mostrarFormulario);
+
+function mostrarFormulario() {
+	banner.style.display = 'none'; 
+	formContainer.style.cssText = 'Opacity: 1; visibility: visible';
+}
