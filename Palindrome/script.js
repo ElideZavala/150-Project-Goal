5884cb8453dc1b3273c722b60859cf6972739e3f
ElideZavala <zavalavinagreelide@gmail.com>
2021-07-30 11:56:15 -0600

    function palidromo

--- Palindrome/script.js
+++ Palindrome/script.js
@@ -1,1 +1,11 @@
-console.log("Hola Mundo")
+const btn    = document.querySelector('.btn');
+const result = document.querySelector('.result');
+
+btn.addEventListener('click', palindromo);
+
+function palindromo() {
+	const word   = document.querySelector('.input-text').value;
+	let len = word.length;
+
+	let start = word.substring(0);
+}
