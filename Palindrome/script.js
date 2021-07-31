a6e778491cd38796e09c48323ceaaf60e29c9fb6
ElideZavala <zavalavinagreelide@gmail.com>
2021-07-30 19:23:48 -0600

    comparacion

--- Palindrome/script.js
+++ Palindrome/script.js
@@ -7,5 +7,16 @@ function palindromo() {
 	const word   = document.querySelector('.input-text').value;
 	let len = word.length;
 
-	let start = word.substring(0);
-}
+	let inicio = word.substring(0, Math.floor(len / 2)).toLowerCase();
+	let final  = word.substring(len - Math.floor(len / 2)).toLowerCase();
+
+	// TODO: Dividir, dar vuelta y unir
+	// let rever   = final.split("").reverse().join("");
+	let rever   = [...final].reverse().join("");
+	
+	if(inicio == rever ) {
+		result.innerHTML = `${word.toUpperCase()} es un palíndromo`
+	} else {
+		result.innerHTML = `${word.toUpperCase()} No es un palíndromo`
+	}
+}	
