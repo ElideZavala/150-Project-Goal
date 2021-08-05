0468395ece9c5d6bccae83a4ebb0754b24269daf
ElideZavala <zavalavinagreelide@gmail.com>
2021-08-05 17:41:29 -0600

    funcion generar Link

--- Google-Drive-Download/script.js
+++ Google-Drive-Download/script.js
@@ -1,9 +1,26 @@
-const gLink = document.createElementById('glink');
-const btn = document.createElementById('btn');
-const descargarLink = document.createElementById('download-link');
+const gLink = document.getElementById('glink');
+const btn = document.getElementById('btn');
+const descargarLink = document.getElementById('download-link');
 
 btn.addEventListener('click', generarLink);
 
 function generarLink(e) {
+	e.preventDefault();
 
+	const gLinkValue  = gLink.value;
+	const id = 'https://drive.google.com/file/d/'
+	
+	const confirmLink = gLink.value.includes(id);
+
+	if (confirmLink == true) {
+		const getDownloadLink = gLink.value.replace(id,'https://drive.google.com/uc?export=download&id=').replace('/view?usp=sharing','')
+		
+		descargarLink.value = getDownloadLink;
+
+		function copyText(target) {
+			if (target.value == "") {
+				alert("Porfavor genera un link de descarga")
+			}
+		}
+	}
 }
