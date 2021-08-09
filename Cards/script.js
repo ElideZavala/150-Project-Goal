69857f81c27fcb5b1ff88adb1977ba1c53893884
ElideZavala <zavalavinagreelide@gmail.com>
2021-08-09 09:07:50 -0600

    animation text

--- Cards/script.js
+++ Cards/script.js
@@ -1,1 +1,21 @@
-console.log("Hola Mundo")
+textEl = document.querySelector('.heading');
+const heading = 'Porfavor Conoce nuestro Equipo de Trabajo';
+let i = 0;
+
+const writeText = () => {
+	if (i < heading.length) {
+		textEl.innerHTML += heading.charAt(i); 
+		i++;
+
+		setTimeout(writeText, 150); 
+	} 
+}
+
+writeText()
+
+
+
+
+
+
+
