102db2ef1898373a0cba290176ab1262000e0031
ElideZavala <zavalavinagreelide@gmail.com>
2021-08-16 19:03:36 -0600

    peticion fetche a la api exitosa

--- Weather-App/script.js
+++ Weather-App/script.js
@@ -1,7 +1,27 @@
+const search = document.querySelector('.input');
+const btn = document.querySelector('.btn');
 const api = {
 	key : "7d55409dc2603538db966d3600124d25",
- 	base: "api.openweathermap.org/data/2.5/"
+ 	base: "https://api.openweathermap.org/data/2.5/"
+ }
+ 
+ btn.addEventListener('click', getInput);
+
+ function getInput(e) {
+	e.preventDefault();
+	if (e.type == 'click') {
+		getData(search.value);
+	}
  }
 
+ function getData() {
+	 fetch(`${api.base}weather?q=${search.value}&units=metric&appid=${api.key}`)
+	 	.then(data => {
+			 return data.json();
+		 }).then(displayData);		 
+}
 
- 
+
+function displayData(data) {
+	console.log(data);
+}
