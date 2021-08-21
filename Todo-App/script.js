a8d484273ecd7ad4ee5fd8f9b155dfff8a613e82
ElideZavala <zavalavinagreelide@gmail.com>
2021-08-21 10:11:34 -0600

    mensaje

--- Todo-App/script.js
+++ Todo-App/script.js
@@ -3,10 +3,12 @@ const input = document.querySelector('input');
 const btn   = document.querySelector('button');
 const todoList = document.querySelector('.todo-list');
 const clear = document.querySelector('.clear');
+const mensg = document.querySelector('.msg');
 
 // Add elementos a la lista
 
-const addTask = () => {
+const addTask = (e) => {
+	e.preventDefault();
 	const newLi  = document.createElement("li");
 	const delBtn = document.createElement("button"); 
 	
@@ -18,8 +20,20 @@ const addTask = () => {
 		todoList.appendChild(newLi);
 		input.value = "";
 	} else {
-		alert("Por favor escribe una nueva tarea")
+		mensaje("Por favor escribe una nueva tarea")
 	}
 }
 
 btn.addEventListener("click", addTask);
+
+function mensaje(msg) {
+	const parrafo = document.createElement('p')
+	parrafo.innerText = msg;
+	parrafo.style.textAlign = 'center';
+	parrafo.style.background = '#2EE0AE';
+	mensg.appendChild(parrafo);
+
+	setTimeout(() => {
+		parrafo.remove();
+	}, 3000);
+}
