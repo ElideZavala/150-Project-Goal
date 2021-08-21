902798141b9474e0f9e792a44bb1c902a82800fd
ElideZavala <zavalavinagreelide@gmail.com>
2021-08-20 19:26:33 -0600

    agregar elementos a la lista

---
+++ Todo-App/script.js
@@ -0,0 +1,22 @@
+// Variables 
+const input = document.querySelector('input');
+const btn   = document.querySelector('button');
+const todoList = document.querySelector('.todo-list');
+const clear = document.querySelector('.clear');
+
+// Add elementos a la lista
+const addTask = () => {
+	const newLi  = document.createElement('li');
+	const delBtn = document.createElement('button'); 
+
+	delBtn.innerHTML = `<i class="fas fa-trash-alt"></i>`;
+
+	if(input.value !== "") {
+		newLi.textContent = input.value;
+		newLi.appendChild(delBtn);
+		todoList.appendChild(newLi);
+		input.value = "";
+	} else {
+		
+	}
+}
