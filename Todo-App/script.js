a636cbb14357d9abdf19052d31adfa19c743807f
ElideZavala <zavalavinagreelide@gmail.com>
2021-08-20 20:08:52 -0600

    end-style

--- Todo-App/script.js
+++ Todo-App/script.js
@@ -5,18 +5,21 @@ const todoList = document.querySelector('.todo-list');
 const clear = document.querySelector('.clear');
 
 // Add elementos a la lista
+
 const addTask = () => {
-	const newLi  = document.createElement('li');
-	const delBtn = document.createElement('button'); 
-
+	const newLi  = document.createElement("li");
+	const delBtn = document.createElement("button"); 
+	
 	delBtn.innerHTML = `<i class="fas fa-trash-alt"></i>`;
-
-	if(input.value !== "") {
+	
+	if (input.value !== "") {
 		newLi.textContent = input.value;
 		newLi.appendChild(delBtn);
 		todoList.appendChild(newLi);
 		input.value = "";
 	} else {
-		
+		alert("Por favor escribe una nueva tarea")
 	}
 }
+
+btn.addEventListener("click", addTask);
