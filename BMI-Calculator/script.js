e7eaf23eea5efb77edb39e9011417550f75ea8e7
ElideZavala <zavalavinagreelide@gmail.com>
2021-09-02 08:13:30 -0600

    variables

---
+++ BMI-Calculator/script.js
@@ -0,0 +1,21 @@
+// IMC - Kg/m2
+// Normal -18.5 -24.9 
+// Underweight(Desnutrido) - < 18.5 - 24.9
+// Overweight(Sobrepeso) 25 - 29.9 
+// Obese - 30  and above
+// Que es el IMC? 
+// El índice de masa corporal (IMC) es un método utilizado para estimar la cantidad de grasa corporal que tiene una persona, y determinar por tanto si el peso está dentro del rango normal, o por el contrario, se tiene sobrepeso o delgadez. El índice de masa corporal (IMC) es un método utilizado para estimar la cantidad de grasa corporal que tiene una persona, y determinar por tanto si el peso está dentro del rango normal, o por el contrario, se tiene sobrepeso o delgadez. 
+
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+const reset = document.querySelector(".reset");
+
+btn.addEventListener('click', calcularIMC);
+
+function calcularIMC(e) {
+	e.preventDefault()
+
+
+	
+	console.log('quio');
+}
