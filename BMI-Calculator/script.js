8a30d70788e46a433e514a6685f11001e03acb17
ElideZavala <zavalavinagreelide@gmail.com>
2021-09-03 16:20:41 -0600

    solve problem

--- BMI-Calculator/script.js
+++ BMI-Calculator/script.js
@@ -11,13 +11,14 @@ const result = document.querySelector(".result");
 const reset = document.querySelector(".reset");
 
 btn.addEventListener('click', calcularIMC);
+reset.addEventListener("click", resetForm);
 
 function calcularIMC(e) {
 	e.preventDefault()
 	let height = document.querySelector('.height').value;
 	let weight = document.querySelector('.weight').value;	
 	
-	// Validate Input
+	// Validar el Input
 	if (height === "" || isNaN(height)) {
 		return (result.innerHTML = "Favor de escribir un altura valida &#128591" );
 	} else if (weight === "" || isNaN(weight)) {
@@ -27,5 +28,29 @@ function calcularIMC(e) {
 		height = height / 100;
 		let imc = (weight / Math.pow(height, 2)).toFixed(2); // Redondeamos a dos decimales. 
 		console.log(imc);
-	}	
+
+		if (imc < 18.5) {
+			showResult(`Bajo Peso: <span>${imc}</span>`, "orange");
+		} else if (imc >= 18.5 && imc < 24.9) {
+			showResult(`Normal: <span>${imc}</span>`, "green");
+		} else if (imc >= 25.0 && imc < 29.9) {
+			showResult(`Sobre Peso: <span>${imc}</span>`, "blue");
+		} else {
+			showResult(`Obesidad: <span>${imc}</span>`, "red");
+		}		
+	}
+	result.style.display = "block";
+	reset.style.display = "block"		
+}
+
+function showResult(val, color) {
+	result.style.backgroundColor = color;
+	return (result.innerHTML = val);
+}
+
+function resetForm() {
+	const form = document.querySelector("form");
+	form.reset();
+	reset.style.display = "none";
+	result.style.display = "none";
 }
