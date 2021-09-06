9ec0107eccecf7371b59744f8ff0125a3c28755f
ElideZavala <zavalavinagreelide@gmail.com>
2021-09-06 17:24:26 -0600

    maquetacion

---
+++ Temperature-Converter/script.js
@@ -0,0 +1,5 @@
+const celcius = document.querySelector('.celcius');
+const fahrenheit = document.querySelector('.fahrenheit');
+const kelvin = document.querySelector('.kelvin');
+const form = document.querySelector('form');
+
