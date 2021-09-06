ee532026da7b4a214848acb1bce0f6c82a43c9a9
ElideZavala <zavalavinagreelide@gmail.com>
2021-09-05 21:19:43 -0600

    toFixed() para contar con dos decimales

--- Weight-Converter/script.js
+++ Weight-Converter/script.js
@@ -9,21 +9,27 @@ form.addEventListener('input', convertWeight);
 function convertWeight(e) {
 	if (e.target.classList.contains("pounds")) {
 		let x = e.target.value;
-		kilograms.value = 
-		grams.value =  
-		ounces.value = 
+		kilgms.value = (x / 2.2046).toFixed(2);
+		grams.value  = (x / 0.0022046).toFixed(2);
+		ounces.value = (x * 16).toFixed(2);
 	}
 	if (e.target.classList.contains("kilograms")) {
 		let x = e.target.value;
-		console.log(x)
+		pounds.value = (x * 2.2045).toFixed(2);
+		grams.value  = (x * 1000).toFixed(2);
+		ounces.value = (x * 35.274).toFixed(2);
 	}
 	if (e.target.classList.contains("grams")) {
 		let x = e.target.value;
-		console.log(x)
+		kilgms.value = (x / 1000).toFixed(2); 
+		pounds.value = (x * 0.0022046).toFixed(2);
+		ounces.value = (x * 0.035274).toFixed(2);
 	}
 	if (e.target.classList.contains("ounces")) {
 		let x = e.target.value;
-		console.log(x)
+		kilgms.value = (x / 35.274).toFixed(2);
+		grams.value  = (x / 0.035274).toFixed(2);
+		pounds.value = (x / 0.0625).toFixed(2);
 	}
 }
 
