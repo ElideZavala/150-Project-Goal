bbdb8bce4c3b393648974135a6e52fe265af0218
ElideZavala <zavalavinagreelide@gmail.com>
2021-09-06 18:13:18 -0600

    Convertidor de Temperatura

--- Temperature-Converter/script.js
+++ Temperature-Converter/script.js
@@ -1,4 +1,4 @@
-const celcius = document.querySelector('.celcius');
+const celsius = document.querySelector('.celsius');
 const fahrenheit = document.querySelector('.fahrenheit');
 const kelvin = document.querySelector('.kelvin');
 const form = document.querySelector('form');
@@ -8,6 +8,17 @@ form.addEventListener('input', convertWeight);
 function convertWeight(e) {
 	if(e.target.classList.contains("celsius")) {
 		let x = e.target.value;
-		console.log(x);
+		fahrenheit.value = (x * 1.8 + 32).toFixed(2);
+		kelvin.value = (x + 273.15);
+	}
+	if(e.target.classList.contains("fahrenheit")) {
+		let x = e.target.value;
+		celsius.value = ((x - 32) / 1.8).toFixed(2);
+		kelvin.value = ((x -32) / 1.8 + 273.15).toFixed(2);
+	}
+	if(e.target.classList.contains("kelvin")) {
+		let x = e.target.value;
+		fahrenheit.value = ((x - 273.15) * 1.8 + 32).toFixed(2);
+		celsius.value = (parseFloat(x) - 273.15).toFixed(2);
 	}
 }
