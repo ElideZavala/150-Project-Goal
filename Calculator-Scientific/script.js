f1b185f00da7964b3ea877a074c4adf6b014f177
ElideZavala <zavalavinagreelide@gmail.com>
2021-09-08 17:12:38 -0600

    new

---
+++ Calculator-Scientific/script.js
@@ -0,0 +1,58 @@
+// let calculatorButtons = [];
+
+// const btnsPerRow = 8;
+// let addedBtns = 0;
+
+// calculatorButtons.forEach( button => {
+	
+
+// 	if(addedBtns % btnsPerRow == 0)
+// 	inputElement.innerHTML += 
+// 		`<div class="row"></div>`;
+	
+// 	row = document.querySelector(".row");
+// 	row.innerHTML += 
+// 		`<button id=${button.name}>
+// 			${button.symbol}
+// 		</button>`;
+	
+// 		addedBtns++;
+
+// });
+
+// // {
+// // 	name: "delete",
+// // 	symbol: "=>",
+// // 	formula: false,
+// // 	type: "key"
+// // }
+// // {
+// // 	name: 8,
+// // 	symbol: 8,
+// // 	formula: 8,
+// // 	type: "number"
+// // }
+// // {
+// // 	name: "multiplication",
+// // 	symbol: "x",
+// // 	formula: "*",
+// // 	type: "operator"
+// // }
+// // {
+// // 	name: "calculate",
+// // 	symbol: "=",
+// // 	formula: "=",
+// // 	type: "calculate"
+// // }
+// // {
+// // 	name: "cos",
+// // 	symbol: "cos",
+// // 	formula: "Math.cos",
+// // 	type: "trigo_funtion"
+// // }
+// // {
+// // 	name: "log",
+// // 	symbol: "log",
+// // 	formula: "Math.log(10)",
+// // 	type: "math_funtion"
+// // }
