8f58ec685acde4a5cca383d46b54f3555484412c
ElideZavala <zavalavinagreelide@gmail.com>
2021-09-09 18:28:00 -0600

    readme

---
+++ Calculator-Scientific/README.md
@@ -0,0 +1,12 @@
+# calculadora 
+`JavaScript && CSS`
+
+## vista 
+ 
+ - Falta darle estilo y agregarle temas
+ - Temas modo oscuro y otros de cambios de color.
+ - Crear function para quitar el NaN. 
+ 
+ [Ir a la pagina](https://sleepy-hopper-556dc9.netlify.app/)
+
+
--- Calculator-Scientific/script.js
+++ Calculator-Scientific/script.js
@@ -2,3 +2,9 @@ function del() {
 	let x = document.getElementById('screen').value;
 	document.getElementById('screen').value = x.substr(0, x.length - 1);
 }
+
+const screen = document.querySelector('screen');
+
+screen.addEventListener('input', (e) => {
+	e.target.value;
+});
