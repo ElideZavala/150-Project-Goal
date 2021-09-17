ce001d209077979bb1e4ee202a2316a4b9fbce32
ElideZavala <zavalavinagreelide@gmail.com>
2021-09-16 18:32:33 -0600

    peticion fetch de la letra de la cancion

--- Lyrics-Search-App/script.js
+++ Lyrics-Search-App/script.js
@@ -3,7 +3,7 @@ const form = document.getElementById('form');
 const search = document.getElementById('search');
 const result = document.getElementById('result');
 
-const apiURL = "http://api.lyrics.ovh";
+const apiURL = "https://api.lyrics.ovh";
 
 // Obtener el valor delinput 
 form.addEventListener('submit', e => {
@@ -20,16 +20,50 @@ form.addEventListener('submit', e => {
 // Creamos la function de Busqueda 
 async function beginSearch(searchValue) {
 	const searchResult = await fetch(`${apiURL}/suggest/${searchValue}`);
-	const data = await searchResult.json;
-	console.log(data);
-	// displayData(data);
+	const data = await searchResult.json();
+	// console.log(data);
+	displayData(data);
 }
 
 // Mostrar Resultado de la Busqueda. 
-// function displayData(data) {
-// 	result.innerHTML = `
-// 		<ul class="songs">
-		
-// 		</ul>
-// 	`
-// }
+function displayData(data) {
+	result.innerHTML = `
+		<ul class="songs">
+			${data
+				.data.map(song => `
+							<li>
+								<div>
+									<strong> ${song.artist.name}</strong> - ${song.title}	
+								</div>
+								<span data-artist="${song.artist.name}"
+								data-songtitle="${song.title}">Obtener Letra</span>
+							</li>	
+				`)
+				.join('')}
+		</ul>
+	`
+}
+
+// Obtener la Function de las Letras
+result.addEventListener('click', e => {
+	const clickedElement = e.target;
+
+	// Boton para Obtener la Letra
+	if(clickedElement.tagName === 'SPAN') {
+		const artist = clickedElement.getAttribute('data-artist');
+		const songTitle = clickedElement.getAttribute('data-songtitle');
+
+		getLyrics(artist, songTitle);
+	}
+})
+
+async function getLyrics(artist, songTitle) {
+	const response = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+
+	const data = await response.json();
+
+	const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+	result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle} </h2>
+	<p>${lyrics}</p>`
+}                
