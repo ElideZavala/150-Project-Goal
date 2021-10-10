93c54e938ee0720074109eaa1c5ba139f0c5691f
ElideZavala <zavalavinagreelide@gmail.com>
2021-10-10 09:46:42 -0600

    function CubesTurn

--- Cube3D/script.js
+++ Cube3D/script.js
@@ -1,10 +1,28 @@
 const leftArrow = document.querySelector('.left-arrow');
+const rightArrow = document.querySelector('.right-arrow');
 const cubes = document.querySelectorAll('.cube');
 
-leftArrow.addEventListener('click', () => {
-	
-	Array.from(cubes).forEach(cube => {
-		cube.style.transform = 'rotateY(90deg)'
+let x = 0;
+
+function cubesTurn() {
+	Array.from(cubes).forEach( cube => {
+		cube.style.transform = `rotateY(${x}deg)`
+		console.log(x)
 	})
-	
+} 
+
+leftArrow.addEventListener('click', () => {
+	x += 90;
+	cubesTurn();
 });
+
+leftArrow.addEventListener('mouseover', () => {
+	x += 25;
+	cubesTurn();
+});
+
+rightArrow.addEventListener('click', () => {
+	x -= 90;
+	cubesTurn();
+});
+
