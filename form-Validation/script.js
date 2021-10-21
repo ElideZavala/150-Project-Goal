344d0dc05350dab0376e9d0e521bea83a7289a67
ElideZavala <zavalavinagreelide@gmail.com>
2021-10-20 18:16:28 -0600

    function correct

--- form-Validation/script.js
+++ form-Validation/script.js
@@ -3,12 +3,16 @@ const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
+const buttons = document.querySelector("button");
 
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 	checkRequired([username, email, password, password2]);
 	checkLength(username, 5, 16);
 	checkLength(password, 8, 16);
+	checkEmail(email);
+	matchPassword(password, password2);
+	// correct();
 });
 
 // Checar los campos requeridos
@@ -41,10 +45,23 @@ function checkLength(input, min, max) {
 	}
 }
 
-//Validar Email
+// Validar Email
 function checkEmail(input) {
-	
+	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if (re.test(input.value.trim())) {
+		showSuccess(input);
+	} else {
+		showError(input, "El correo no es valido");
+	}
 }
+
+// Checar Contraseña
+function matchPassword(input1, input2) {
+	if (input1.value !== input2.value) {
+		showError(input2, "Contraseñas no coinciden");
+	}
+}
+
 
 // Mostrar Mensaje de exito 
 function showSuccess(input) {
@@ -55,4 +72,10 @@ function showSuccess(input) {
 // GetFieldName 
 function getFieldName(input) {
 	return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function correct() {
+	const msg = buttons.parentElement;
+	msg.innerText = "Has Completado tu Registro Correctamente"
+	msg.classList.add("login");
 }
