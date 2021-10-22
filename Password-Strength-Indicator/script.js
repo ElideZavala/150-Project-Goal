18a533ec04b5c0e6e49a3d65325cd19af72679cc
ElideZavala <zavalavinagreelide@gmail.com>
2021-10-22 17:11:07 -0600

    variables

---
+++ Password-Strength-Indicator/script.js
@@ -0,0 +1,11 @@
+let state    = document.getElementById("password");
+let password = document.getElementById("password");
+let passwordStrength = document.getElementById("password-strength");
+
+let lowUpperCase = document.querySelector(".low-upper-case i"),
+	number = document.querySelector(".number i"),
+	specialChar = document.querySelector(".special-Char i"),
+	eightChar = document.querySelector(".eight-char i"),
+	showPassword = document.querySelector(".show-pass"),
+	eyeIcon = document.querySelector("#eye"),
+
