961114c48d8ad3e47f1658b2e8f07e46454a059e
ElideZavala <zavalavinagreelide@gmail.com>
2021-11-04 10:35:40 -0700

    class speechRecognition

--- Speech-to-text/script.js
+++ Speech-to-text/script.js
@@ -9,7 +9,32 @@ const speechRecognition = window.speechRecognition || window.webkitSpeechRecogni
 // Verificar la compativilidad con otros navegadores. 
 if(speechRecognition) {
 	console.log("Speech Recognition es soportado");
+
+	const recognition = new speechRecognition();
+
+	micBtn.addEventListener('click', micBtnClicked);
+
+	function micBtnClicked(e) {
+		e.preventDefault();
+		if (micBtn.classList.contains("fa-microphone")) {
+			recognition.start(); // Empezar 
+		} else {
+			recognition.stop(); // Parar
+		}
+	}
+
+	recognition.addEventListener("start", () => {
+		micBtn.classList.remove("fa-microphone");
+		micBtn.classList.add("fa-microphone-slash");
+		instruction.textContent = "Grabando...",
+		searchInput.focus();
+		console.log("Speech Recognition Enabled");
+	})
+
+	// recognition.onstart = function() {
+
+	// }
 } else {
 	// Ocultamos el boton de voz en dado caso de que no sea soportado
-	speechBtnDiv.style.visibility = "hidden"
+	speechBtnDiv.style.visibility = "hidden";
  }
