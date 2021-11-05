2c7fadc5f8dea0971c05f30d6f2422435ea9da1f
ElideZavala <zavalavinagreelide@gmail.com>
2021-11-05 08:58:28 -0700

    end and content

--- Speech-to-text/script.js
+++ Speech-to-text/script.js
@@ -4,25 +4,24 @@ const speechBtnDiv = document.getElementById("speech-btn");
 const micBtn = document.querySelector(".btn .fas");
 const instruction  = document.querySelector(".instruction");
 
-const speechRecognition = window.speechRecognition || window.webkitSpeechRecognition
+const speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition ;
 
 // Verificar la compativilidad con otros navegadores. 
 if(speechRecognition) {
 	console.log("Speech Recognition es soportado");
 
 	const recognition = new speechRecognition();
-
-	micBtn.addEventListener('click', micBtnClicked);
-
+	micBtn.addEventListener("click", micBtnClicked);
 	function micBtnClicked(e) {
 		e.preventDefault();
 		if (micBtn.classList.contains("fa-microphone")) {
 			recognition.start(); // Empezar 
 		} else {
-			recognition.stop(); // Parar
+			recognition.stop(); 
 		}
 	}
 
+	// Iniciar el reconociminto de voz
 	recognition.addEventListener("start", () => {
 		micBtn.classList.remove("fa-microphone");
 		micBtn.classList.add("fa-microphone-slash");
@@ -31,10 +30,29 @@ if(speechRecognition) {
 		console.log("Speech Recognition Enabled");
 	})
 
-	// recognition.onstart = function() {
+	// Parar el reconociminto de voz
+	recognition.addEventListener("end", () => {
+		micBtn.classList.remove("fa-microphone-slash");
+		micBtn.classList.add("fa-microphone");
+		instruction.textContent = "Da click en el icono del microfono para comenzar",
+		searchInput.focus();
+		console.log("Speech Recognition Disabled");
+	});
 
-	// }
-} else {
+	// Ontener resultados del reconocimiento de voz. 
+	let content = "";
+	recognition.addEventListener("result", e => {
+		console.log(e)
+		const current = e.resultIndex;
+		const transcript = e.result[current][0].transcript;
+		content += transcript;
+		searchInput.value = content;
+		searchInput.focus();
+	}); 
+
+
+} 
+else {
 	// Ocultamos el boton de voz en dado caso de que no sea soportado
 	speechBtnDiv.style.visibility = "hidden";
  }
