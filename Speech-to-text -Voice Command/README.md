3d91b89ab57c0e261712e14a23941a691ce498f8
ElideZavala <zavalavinagreelide@gmail.com>
2021-11-06 09:57:15 -0700

    structura

---
+++ Speech-to-text -Voice Command/README.md
@@ -0,0 +1,22 @@
+# Convertidor de voz a texto
+`JavaScript && Bootstrap`    
+    
+## Vista de la Página
+![image](vista.png)
+
+:computer: [Ir a la Página](https://jovial-nobel-86aba6.netlify.app/) :computer:
+
+#### ``Codigo para verificar si el navergador es compatible con  speechRecognition``
+
+    const speechRecognition = window.speechRecognition || window.webkitSpeechRecognition
+
+#### ``Como utilizar SpeechRecognition``
+
+    const recognition = new speechRecognition();  <== Utilizamos los metódos
+
+**Realizamos las peticiones correspondientes** ⏬ :arrow_down:
+
+![image](nota1.jpg) 
+
+
+![image](nota2.jpg) 
---
+++ Speech-to-text -Voice Command/script.js
@@ -0,0 +1,57 @@
+const searchForm   = document.querySelector("#search-form");
+const searchInput  = document.querySelector("#search-input");
+const speechBtnDiv = document.querySelector("speech-btn");
+const micBtn = document.querySelector(".btn .fas");
+const instruction  = document.querySelector(".instruction");
+
+const speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition ;
+
+// Verificar la compativilidad con otros navegadores. 
+if(speechRecognition) {
+	console.log("Speech Recognition es soportado");
+
+	const recognition = new speechRecognition();
+
+	micBtn.addEventListener("click", micBtnClicked);
+	function micBtnClicked(e) {
+		e.preventDefault();
+		if (micBtn.classList.contains("fa-microphone")) {
+			recognition.start(); // Empezar 
+		} else {
+			recognition.stop(); 
+		}
+	}
+
+	// Iniciar el reconociminto de voz
+	recognition.addEventListener("start", () => {
+		micBtn.classList.remove("fa-microphone");
+		micBtn.classList.add("fa-microphone-slash");
+		instruction.textContent = "Grabando...",
+		searchInput.focus();
+		console.log("Speech Recognition Enabled");
+	})
+
+	// Parar el reconociminto de voz
+	recognition.addEventListener("end", () => {
+		micBtn.classList.remove("fa-microphone-slash");
+		micBtn.classList.add("fa-microphone");
+		instruction.textContent = "Da click en el icono del microfono para comenzar",
+		searchInput.focus();
+		console.log("Speech Recognition Disabled");
+	});
+
+	// Ontener resultados del reconocimiento de voz. 
+	recognition.continuous = true;
+	let content = "";
+	recognition.addEventListener("result", e => {
+		console.log(e)
+		const current = e.resultIndex;
+		const transcript = e.results[current][0].transcript;
+		content += transcript;
+		searchInput.value = content;
+		searchInput.focus();
+	}); 
+} else {
+	// Ocultamos el boton de voz en dado caso de que no sea soportado
+	speechBtnDiv.style.visibility = "hidden";
+ }
