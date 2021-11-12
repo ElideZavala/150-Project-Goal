258df9ff7466c89faceab1b0144d5f0c8c057ba5
ElideZavala <zavalavinagreelide@gmail.com>
2021-11-11 19:18:30 -0700

    function type

---
+++ Type-Writing-Effect-Proyect/script.js
@@ -0,0 +1,49 @@
+const typedWord = document.querySelector(".typed-word");
+const cursor    = document.querySelector(".cursor");
+
+const wordArray = ["Amante de la tecnología", "Creador de Páginas Web", "FullStack Js"];
+
+let wordArrayIndex = 0;
+let letterIndex = 0;
+
+const typingDelay = 200;
+const erasingDelay = 100;
+const newWordDelay = 2000;
+
+// Typing Function
+function type() {
+	if(letterIndex < wordArray[wordArrayIndex].length) {
+		typedWord.textContent += wordArray[wordArrayIndex].charAt(letterIndex);
+		letterIndex++;
+
+		// Ejecutar la function type en cierto tiempo.
+		setTimeout(type, typingDelay);
+	} else {
+		setTimeout(erase, typingDelay);
+	}
+}
+
+type();
+
+
+
+
+
+
+
+
+
+
+
+// wordArray.forEach((word, index) => {
+// 	console.log(`${index} => ${word}`);
+// });
+
+// const word = "Developer";
+// const result = word.charAt(1);
+// console.log(result);
+
+// const word = "Developer";
+// const result = word.substring(0, 3);
+// console.log(result);
+
