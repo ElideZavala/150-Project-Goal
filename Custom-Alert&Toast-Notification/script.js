f9028f022c79cd8b8f24fc6ecff9df5fda337664
ElideZavala <zavalavinagreelide@gmail.com>
2021-11-16 16:46:33 -0700

    let

---
+++ Custom-Alert&Toast-Notification/script.js
@@ -0,0 +1,6 @@
+const main = document.querySelector(".main");
+const alertBox = document.querySelector(".alert");
+const exclamationIcon = document.querySelector(".fa-exclamation-circle");
+const msg = document.querySelector(".msg");
+const closeBtn = document.querySelector(".close-btn");
+const closeIcon = document.querySelector(".fa-time")
