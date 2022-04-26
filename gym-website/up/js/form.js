7d3725316213a324886c7d71c8b0cf86ea8a8c41
ElideZavala <zavalavinagreelide@gmail.com>
2022-04-26 16:32:20 -0500

    part-1 funtionality form

--- gym-website/up/js/form.js
+++ gym-website/up/js/form.js
@@ -31,12 +31,17 @@ let current = 1;
 
 // First Next Button Event Listener
 firstNextBtn.addEventListener('click', () => {
-	slidePage.style.marginLeft = '-33%'
+	slidePage.style.marginLeft = '-33%';
+	progressStep[current - 1].classList.add('active');
+	progressNumber[current - 1].classList.add('active');
+	progressTick[current - 1].classList.add('active');
+	current += 1;
 });
 
 // Second Next Button Event Listener
 secondNextBtn.addEventListener('click', () => {
 	slidePage.style.marginLeft = '-100%'
+	// progressStep[current - 1].classList.add('active');
 });
 
 // Third Next Button Event Listener
@@ -44,22 +49,33 @@ thirdNextBtn.addEventListener('click', () => {
 	slidePage.style.marginLeft = '-200%'
 });
 
+// Submit Button Event Listener
+submitBtn.addEventListener('click', () => {
+	swal("Good done!", "Your registration has been successful!", "success");
+	setTimeout(() => {
+		location.reload();
+	}, 1500);
+})
+
 // *-*-*-*-*-*-*-*-*-*-*-* Previous Btns Events *-*-*-*-*-*-*-*-*-*-*-*-*
 
 // First Prev Button Event Listener
 firstPrevBtn.addEventListener('click', () => {
-	slidePage.style.marginLeft = '0'
+	slidePage.style.marginLeft = '0';
+	progressStep[current - 2].classList.remove('active');
+	progressNumber[current - 2].classList.remove('active');
+	progressTick[current - 2].classList.remove('active');
+	current -= 1;
 });
 
 // Second Prev Button Event Listener
 secondPrevBtn.addEventListener('click', () => {
 	slidePage.style.marginLeft = '-33%'
+	progressStep[current - 3].classList.remove('active');
+	progressNumber[current - 3].classList.remove('active');
+	progressTick[current - 3].classList.remove('active');
+	current += 2;
 });
-
-// Submit Button Event Listener
-submitBtn.addEventListener('click', () => {
-	swal("Good job!", "Your registration has been successful!", "success");
-})
 
 // Second Prev Button Event Listener
 thirdPrevBtn.addEventListener('click', () => {
